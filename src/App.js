2400fce20ba30c902710072c113b338aa702a860
Ryan Paolo Joson <bantora.1011@gmail.com>
2023-06-28 21:00:15 +0800

    code and test scoops options images from server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import SummaryForm from './pages/summary/SummaryForm';
+import SummaryForm from "./pages/summary/SummaryForm";
+import Options from "./pages/entry/Options";
 
 function App() {
   return (
     <div className='App'>
       <SummaryForm />
+      <Options optionType='scoops' />
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import ScoopOptions from "./ScoopOptions";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((_err) => {
+        //TODO
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOptions : null;
+
+  console.log(items.name);
+
+  const optionItems = items.map((value) => (
+    <ItemComponent
+      key={value.name}
+      name={value.name}
+      imagePath={value.imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
